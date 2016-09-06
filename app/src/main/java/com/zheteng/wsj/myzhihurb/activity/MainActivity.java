82c610b2cc0c5ce4f60beb15037cda213dfb22ce
wangshijia <wsj2072@163.com>
2016-09-06 14:26:46 +0800

    项目构建

---
+++ app/src/main/java/com/zheteng/wsj/myzhihurb/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.zheteng.wsj.myzhihurb.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zheteng.wsj.myzhihurb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
